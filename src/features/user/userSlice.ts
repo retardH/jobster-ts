e4154f503ebb83f2eeda7178bbb86af579891a0b
htetzarni <loowmaad@gmail.com>
2023-10-07 17:25:03 +0630

    register function developing

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,13 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
-interface UserSliceInitial {
+interface UserSlice {
     isLoading: boolean;
     user: any;
 }
-const initialState: UserSliceInitial = {
+const initialState: UserSlice = {
     isLoading: false,
     user: null,
 }
+
+export const registerUser = createAsyncThunk(
+    'user/registerUser',
+    async (user, thunkAPI) => {
+        console.log(`Register User: ${user}`, thunkAPI);
+    }
+);
+
+export const loginUser = createAsyncThunk(
+    'user/loginUser',
+    async (user, thunkAPI) => {
+        console.log(`Login User: ${user}`, thunkAPI);
+    }
+)
 
 const userSlice = createSlice({
     name: 'user',
@@ -17,6 +31,6 @@ const userSlice = createSlice({
             console.log('logged');
         }
     }
-})
+});
 
 export default userSlice.reducer;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,75 +1,83 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import Logo from '../components/Logo';
 import FormRow from '../components/FormRow';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../features/store.ts";
+import {loginUser} from "../features/user/userSlice.ts";
 
 const initialState = {
-  name: '',
-  email: '',
-  password: '',
-  isMember: true,
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
 };
 const Register = () => {
-  const [formValues, setFormValues] = useState(initialState);
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    console.log(e.target.value);
-    setFormValues((values) => ({ ...values, [e.target.name]: e.target.value }));
-  };
-  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { name, password, email, isMember } = formValues;
-    if (!email || !password || !isMember || !name) {
-      console.log('Please fill out all fields');
-      toast.warn('Please fill out all fields');
-      return;
-    }
-    console.log('submitted');
-  };
-  const toggleMember = () => {
-    setFormValues((prev) => ({ ...prev, isMember: !prev.isMember }));
-  };
-  return (
-    <Wrapper className="full-page">
-      <form className="form" onSubmit={onSubmit}>
-        <Logo />
-        <h3>{formValues.isMember ? 'Login' : 'Register'}</h3>
-        {!formValues.isMember && (
-          <FormRow
-            name="name"
-            value={formValues.name}
-            handleChange={handleChange}
-            labelText="Name"
-          />
-        )}
-        <FormRow
-          type="email"
-          name="email"
-          value={formValues.email}
-          handleChange={handleChange}
-          labelText="Email"
-        />
-        <FormRow
-          type="password"
-          name="password"
-          value={formValues.password}
-          handleChange={handleChange}
-          labelText="Password"
-        />
-        <button type="submit" className="btn btn-block">
-          submit
-        </button>
-        <p>
-          {!formValues.isMember ? 'Already a member?' : 'Not a member yet?'}
-          <button type="button" className="member-btn" onClick={toggleMember}>
-            {!formValues.isMember ? 'Login' : 'Register'}
-          </button>
-        </p>
-      </form>
-    </Wrapper>
-  );
+    const {user, isLoading} = useSelector((state: RootState) => state.user);
+    const dispatch = useDispatch<AppDispatch>();
+    const [formValues, setFormValues] = useState(initialState);
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        console.log(e.target.value);
+        setFormValues((values) => ({...values, [e.target.name]: e.target.value}));
+    };
+    const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const {name, password, email, isMember} = formValues;
+        if (!email || !password || !isMember || !name) {
+            console.log('Please fill out all fields');
+            toast.warn('Please fill out all fields');
+            return;
+        }
+        if (isMember) {
+            dispatch(loginUser({ email: email, password: password }));
+        }
+        console.log('submitted');
+    };
+    const toggleMember = () => {
+        setFormValues((prev) => ({...prev, isMember: !prev.isMember}));
+    };
+    return (
+        <Wrapper className="full-page">
+            <form className="form" onSubmit={onSubmit}>
+                <Logo/>
+                <h3>{formValues.isMember ? 'Login' : 'Register'}</h3>
+                {!formValues.isMember && (
+                    <FormRow
+                        name="name"
+                        value={formValues.name}
+                        handleChange={handleChange}
+                        labelText="Name"
+                    />
+                )}
+                <FormRow
+                    type="email"
+                    name="email"
+                    value={formValues.email}
+                    handleChange={handleChange}
+                    labelText="Email"
+                />
+                <FormRow
+                    type="password"
+                    name="password"
+                    value={formValues.password}
+                    handleChange={handleChange}
+                    labelText="Password"
+                />
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+                <p>
+                    {!formValues.isMember ? 'Already a member?' : 'Not a member yet?'}
+                    <button type="button" className="member-btn" onClick={toggleMember}>
+                        {!formValues.isMember ? 'Login' : 'Register'}
+                    </button>
+                </p>
+            </form>
+        </Wrapper>
+    );
 };
 
 export default Register;
