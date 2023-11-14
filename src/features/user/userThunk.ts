d796071a727b3ac183e616579e6c4795cb822980
HtetZarni <loowmaad@gmail.com>
2023-11-15 01:49:56 +0630

    modify type file

--- src/features/user/userThunk.ts
+++ src/features/user/userThunk.ts
@@ -1,15 +1,15 @@
-import { LoginPayload, RegisterPayload, ThunkApi } from "../../types";
-import { checkForUnauthorizedResponse, request } from "../../utils/axios";
-import { clearJobsState } from "../jobs/jobsSlice";
-import { logoutUser } from "./userSlice";
+import { LoginPayload, RegisterPayload, ThunkApi } from '../../types';
+import { checkForUnauthorizedResponse, request } from '../../utils/axios';
+import { clearJobsState } from '../jobs/jobsSlice';
+import { logoutUser } from './userSlice';
 
 export const registerUserThunk = async (
   url: string,
   user: RegisterPayload,
-  thunkAPI: ThunkApi,
+  thunkAPI: ThunkApi
 ) => {
   try {
-    const resp: any = await request("post", url, user);
+    const resp: any = await request('post', url, user);
     return resp.data;
   } catch (error: any) {
     return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -19,10 +19,10 @@ export const registerUserThunk = async (
 export const loginUserThunk = async (
   url: string,
   user: LoginPayload,
-  thunkAPI: ThunkApi,
+  thunkAPI: ThunkApi
 ) => {
   try {
-    const resp: any = await request("post", url, user);
+    const resp: any = await request('post', url, user);
     return resp.data;
   } catch (error: any) {
     return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -32,10 +32,10 @@ export const loginUserThunk = async (
 export const updateUserThunk = async (
   url: string,
   user: any,
-  thunkAPI: ThunkApi,
+  thunkAPI: ThunkApi
 ) => {
   try {
-    const response: any = await request("patch", url, user);
+    const response: any = await request('patch', url, user);
     return response.data;
   } catch (err: any) {
     return checkForUnauthorizedResponse(err, thunkAPI);
