31d2953f7ebde8c73e22c75ba0a39339fc18e4e8
htetzarni <loowmaad@gmail.com>
2023-11-16 10:09:28 +0630

    change landing page image

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import Dashboard from "./pages/dashboard";
-import Landing from "./pages/Landing";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./pages/Register";
-import Error from "./pages/Error";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-import Layout from "./components/layout";
-import AllJobs from "./pages/dashboard/AllJobs.tsx";
-import AddJob from "./pages/dashboard/AddJob.tsx";
-import Profile from "./pages/dashboard/Profile.tsx";
-import ProtectedRoute from "./pages/ProtectedRoute.tsx";
+import Dashboard from './pages/dashboard';
+import Landing from './pages/Landing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './pages/Register';
+import Error from './pages/Error';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import Layout from './components/layout';
+import AllJobs from './pages/dashboard/AllJobs.tsx';
+import AddJob from './pages/dashboard/AddJob.tsx';
+import Profile from './pages/dashboard/Profile.tsx';
+import ProtectedRoute from './pages/ProtectedRoute.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer
         position="top-center"
-        autoClose={2000}
-        style={{ zIndex: 9999999 }}
+        autoClose={1500}
+        style={{ zIndex: 99999 }}
       />
       <Routes>
         <Route
@@ -26,8 +26,7 @@ function App() {
             <ProtectedRoute>
               <Layout />
             </ProtectedRoute>
-          }
-        >
+          }>
           <Route index element={<Dashboard />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
